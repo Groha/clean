ae1ed85c600bab10129e552ace058ef15d03a294
Kris <86876291+Groha@users.noreply.github.com>
2023-08-31 17:13:08 +0200

    fixed height on mob

--- app.vue
+++ app.vue
@@ -158,10 +158,10 @@ export default {
     <div class="border border-solid border-gray-300">
       <h1 class="p-5 text-4xl font-bold">График уборки</h1>
       <div class="grid md:grid-cols-5">
-        <input class="border border-gray-300 p-3 m-2 outline-none" type="date" placeholder="Дата..." v-model="currentDate">
-        <input class="border border-gray-300 p-3 m-2 outline-none" type="number" min="1" max="30" placeholder="№ к-ты..." v-model="currentRoom" />
-        <input class="border border-gray-300 p-3 m-2 outline-none" type="number" min="1" max="3" placeholder="Этаж..." v-model="currentFloor" />
-        <input class="border border-gray-300 p-3 m-2 outline-none" type="text" placeholder="Имя..." v-model="currentName" />
+        <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="date" placeholder="Дата..." v-model="currentDate">
+        <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="number" min="1" max="30" placeholder="№ к-ты..." v-model="currentRoom" />
+        <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="number" min="1" max="3" placeholder="Этаж..." v-model="currentFloor" />
+        <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="text" placeholder="Имя..." v-model="currentName" />
         <button @click="addRoom()" class="p-3 border border-gray-300 rounded-sm cursor-pointer bg-white transition-all duration-300 m-2 hover:bg-gray-300">Добавить комнату</button>
       </div>
     </div>
@@ -183,19 +183,19 @@ export default {
       <div class="flex md:flex-col overflow-x-scroll mt-5 md:mt-0 md:overflow-y-scroll">
         <div v-for="todo in filteredSchedule" :key="todo.id">
           <div class="border border-gray-300 border-t-0 grid md:grid-cols-5 min-w-[140px]">
-            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none">{{ todo.date }}</span>
-            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none">{{ todo.room }}</span>
-            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none">{{ todo.floor }}</span>
-            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none">{{ todo.name }}</span>
+            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none min-h-[65px]">{{ todo.date }}</span>
+            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none min-h-[65px]">{{ todo.room }}</span>
+            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none min-h-[65px]">{{ todo.floor }}</span>
+            <span class="border-t md:border-t-0 md:border-l border-gray-300 p-5 md:first:border-none min-h-[65px]">{{ todo.name }}</span>
             <p @click="openModal(todo)" class="m-0 flex justify-center p-5 border-t md:border-t-0 md:border-l border-gray-300 cursor-pointer">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>
             </p>
             <div v-if="showModal" class="w-full h-full fixed inset-0 bg-slate-600">
               <div class="grid bg-white w-full md:w-[40%] mx-auto top-[50%] translate-y-[-50%] relative p-5 pt-10">
-                <input class="border border-gray-300 p-3 m-2 outline-none" type="date" placeholder="Дата..." v-model="modalDate">
-                <input class="border border-gray-300 p-3 m-2 outline-none" type="number" min="1" max="30" placeholder="№ к-ты..." v-model="modalRoom" />
-                <input class="border border-gray-300 p-3 m-2 outline-none" type="number" min="1" max="3" placeholder="Этаж..." v-model="modalFloor" />
-                <input class="border border-gray-300 p-3 m-2 outline-none" type="text" placeholder="Имя..." v-model="modalName" />
+                <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="date" placeholder="Дата..." v-model="modalDate">
+                <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="number" min="1" max="30" placeholder="№ к-ты..." v-model="modalRoom" />
+                <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="number" min="1" max="3" placeholder="Этаж..." v-model="modalFloor" />
+                <input class="border border-gray-300 p-3 m-2 outline-none w-full md:w-auto" type="text" placeholder="Имя..." v-model="modalName" />
                 <button @click="updateRoom()" class="p-3 border border-gray-300 rounded-sm cursor-pointer bg-white transition-all duration-300 m-2 hover:bg-gray-300">Сохранить данные</button>
                 <span @click="showModal = false" class="absolute top-[10px] right-[14px] font-bold cursor-pointer">x</span>
               </div>
